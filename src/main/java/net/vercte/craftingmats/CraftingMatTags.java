5aa44ddf7046e41bf1e562c22f1411f0c1e8a189
vercte <me@vercte.net>
2026-05-16 10:07:57 -0700

    rename tag lol

--- src/main/java/net/vercte/craftingmats/CraftingMatTags.java
+++ src/main/java/net/vercte/craftingmats/CraftingMatTags.java
@@ -5,5 +5,5 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class CraftingMatTags {
-    public static final TagKey<Block> SUPPORTS_MAT = TagKey.create(Registries.BLOCK, CraftingMats.at("supports_block"));
+    public static final TagKey<Block> SUPPORTS_MAT = TagKey.create(Registries.BLOCK, CraftingMats.at("supports_mat"));
 }
